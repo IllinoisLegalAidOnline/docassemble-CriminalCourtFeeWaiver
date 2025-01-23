aaa68c963841e735337394f4afea58654adc24dc
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-01-23 09:07:46 -0600

    updated order template

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.CriminalCourtFeeWaiver',
-      version='1.0.0',
+      version='1.0.2',
       description=('A docassemble extension.'),
       long_description='# docassemble.CriminalCourtFeeWaiver\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -52,9 +52,8 @@ setup(name='docassemble.CriminalCourtFeeWaiver',
       author_email='mnewsted@illinoislegalaid.org',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
-      install_requires=['docassemble.ALToolbox>=0.11.0', 'docassemble.AssemblyLine>=3.1.0', 'docassemble.ILAO>=1.0.99', 'docassemble.InterviewStats>=0.5.0', 'docassemble.PovertyScale>=2024.0.0'],
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.ALToolbox>=0.11.1', 'docassemble.AssemblyLine>=3.2.0', 'docassemble.ILAO>=1.0.99', 'docassemble.InterviewStats>=0.5.0', 'docassemble.PovertyScale>=2024.0.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/CriminalCourtFeeWaiver/', package='docassemble.CriminalCourtFeeWaiver'),
      )
